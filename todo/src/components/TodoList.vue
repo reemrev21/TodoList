9edd123aaa0fb5ead33288f425c579654817d8bc
Seungmin Lee <reemrev21@gmail.com>
2021-05-11 00:15:12 +0900

    store 속성 모듈화

--- todo/src/components/TodoList.vue
+++ todo/src/components/TodoList.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <transition-group name="list" tag="b-list-group">
-      <b-list-group-item v-for="todoItem, index in this.$store.state.todoItems" v-bind:key="todoItem.item" class="d-flex justify-content-between shadow-sm p-2 mb-1 bg-white rounded">
+      <b-list-group-item v-for="todoItem, index in this.storedTodoItems" v-bind:key="todoItem.item" class="d-flex justify-content-between shadow-sm p-2 mb-1 bg-white rounded">
         <span>
           <b-icon icon="square" v-if="!todoItem.completed" scale="1" class="checkBtn" 
             v-bind:class="{getCheck: todoItem.completed}" 
-            v-on:click="toggleComplete(todoItem, index)">
+            v-on:click="toggleComplete({todoItem, index})">
           </b-icon>
           <b-icon icon="check-square-fill" v-if="todoItem.completed" scale="1" class="checkBtn" 
             v-bind:class="{getCheck: todoItem.completed}" 
-            v-on:click="toggleComplete(todoItem, index)">
+            v-on:click="toggleComplete({todoItem, index})">
           </b-icon>
         </span> 
         <span class="list" v-bind:class="{textCompleted : todoItem.completed}"> 
         <b> {{ todoItem.item }} </b>
         </span>
-        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+        <span class="removeBtn" v-on:click="removeTodo({todoItem, index})">
           <b-icon icon="trash-fill"></b-icon>
         </span>
       </b-list-group-item>
@@ -24,18 +24,20 @@
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex'
+
 export default {
   methods: {
-    removeTodo(todoItem, index) {
-      this.$store.commit('removeOneItem', { todoItem, index })
-      // store를 사용하기 위해서는 todoitem, index를 객체로 변환해야함
-      // this.$store.commit('removeOneItem', todoItem, index)
-    },
+    ...mapMutations({
+      removeTodo: 'removeOneItem',
+      toggleComplete : 'toggleOneItem'
+    })
 
-    toggleComplete(todoItem, index) {
-      this.$store.commit('toggleOneItem', { todoItem, index })
-    },
   },
+  
+  computed: {
+    ...mapGetters(['storedTodoItems'])
+  }
 }
 </script>
 
--- todo/src/store/index.js
+++ todo/src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as mutations from './mutations'
 
 Vue.use(Vuex)
 
@@ -23,31 +25,8 @@ export default new Vuex.Store({
   state: {
     todoItems: storage.fetch()
   },
-  mutations: {
-    addOneItem(state, todoItem) {
-      const obj = {completed: false, item: todoItem};
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      state.todoItems.push(obj);
-    },
-
-    removeOneItem(state, payload) {
-      localStorage.removeItem(payload.todoItem.item)
-      state.todoItems.splice(payload.index, 1);
-    },
-
-    toggleOneItem(state, payload){
-      // 로컬 스토리지 데이터 갱신
-      state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-      localStorage.removeItem(payload.todoItem.item);
-      localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem))
-    },
-
-    clearAllItems(state) {
-      localStorage.clear();
-      // 배열을 비워주어야 화면에 갱신된다.
-      state.todoItems = [];
-    }
-  },
+  getters,
+  mutations,
   actions: {
   },
   modules: {
