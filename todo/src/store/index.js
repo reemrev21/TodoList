2dccfd94c077a4e825b79916884747d9d11e5538
Seungmin Lee <reemrev21@gmail.com>
2021-05-11 00:27:33 +0900

    store 모듈화

--- todo/src/store/index.js
+++ todo/src/store/index.js
@@ -1,34 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as getters from './getters'
-import * as mutations from './mutations'
+import todoApp from './modules/todoApp'
 
 Vue.use(Vuex)
 
-const storage = {
-  fetch() {
-    // 기존에 this.todoItems를 사용했는데 블럭 문제로 접근이 안되니까
-    // arr 배열 만들어서 push 해준다.
-    const arr = []
-    if(localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
-        }
-      }
-    }
-    return arr;
-  },
-}
-
 export default new Vuex.Store({
-  state: {
-    todoItems: storage.fetch()
-  },
-  getters,
-  mutations,
-  actions: {
-  },
   modules: {
+    todoApp
   }
 })
